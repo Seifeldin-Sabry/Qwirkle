de17053e5173a66cf9fa75605aa6271dcb1ebda3
Sakis <sakis.stefanidis@student.kdg.be>
2022-04-26 13:48:12 +0200

    Test push

--- Qwirkle_Beta_Version/src/qwirkle/model/PlayerSession.java
+++ Qwirkle_Beta_Version/src/qwirkle/model/PlayerSession.java
@@ -74,7 +74,6 @@ public class PlayerSession implements List<Turn> {
         }
     }
 
-
     public long getTotalTimeSpent() {
         OptionalLong timeSpent = turnsPlayed.stream().mapToLong(Turn::getTurnDuration).reduce(Long::sum);
         try {
